48753f0f1290f32ace33fc2c4c57ef78bbc2f1ef
Paula Ortiz <p.ortizf.2023@alumnos.urjc.es>
2024-04-20 16:27:26 +0200

    Mejorado estructura, menú para móvil y página web en general

---
+++ javaS.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".menu");
+const abrirMenu = document.querySelector(".abrirMenu");
+
+function toggleMenu() {
+	menu.classList.toggle("menuCerrado");
+}
+
+abrirMenu.addEventListener("click", toggleMenu);
