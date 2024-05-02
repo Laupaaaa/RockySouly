2d2b6cfdce086d6d846cc59cd6e33245a4f92d22
Paula Ortiz <p.ortizf.2023@alumnos.urjc.es>
2024-05-02 20:39:13 +0200

    Añadido comienzo de FAQ, Equipo y Autores

--- javaS.js
+++ javaS.js
@@ -1,8 +1,20 @@
 const menu = document.querySelector('.menu');
 const abrirMenu = document.querySelector('.abrirMenu');
+const preguntas = document.querySelectorAll(".preguntando");
 
 function toggleMenu() {
 	menu.classList.toggle('menuCerrado');
 }
-
 abrirMenu.addEventListener('click', toggleMenu);
+
+preguntas.forEach((pregunta) => {
+	pregunta.addEventListener('click', () => {
+		cerraRespuesta(); 
+		pregunta.nextElementSibling.classList.add('verRespuestas');
+	}); 
+});
+function cerraRespuesta() {
+	preguntas.forEach((pregunta) => {
+		pregunta.nextElementSibling.classList.remove('verRespuestas')
+	});
+}
